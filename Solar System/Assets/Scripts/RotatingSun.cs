fc9d02ab9a39013fd250de40e285a01345bc6d2f
Jon Larriba <257492421+jonLarriba333@users.noreply.github.com>
2026-03-26 09:04:30 +0100

    Implemented so that the sun rotates.
    The sun now rotates over its own axis.

--- Solar System/Assets/Scripts/RotatingSun.cs
+++ Solar System/Assets/Scripts/RotatingSun.cs
@@ -3,14 +3,15 @@ using UnityEngine;
 public class RotatingSun : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float sunSpeed;
     void Start()
     {
-        
+        sunSpeed = -200;
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.rotation *= Quaternion.Euler(0, 0, 0);
+        gameObject.transform.rotation *= Quaternion.Euler(0, 1 * Time.deltaTime * sunSpeed, 0);
     }
 }
